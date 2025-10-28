85b3f38dc5bbe17089646be67fdc6989d8340e0d
Maxx Crawford <maxx.crawford@gmail.com>
2025-10-28 11:24:53 -0700

    Update secrets.py

--- badge/secrets.py
+++ badge/secrets.py
@@ -1,8 +1,8 @@
 # Update with your 2.4Ghz WiFi details
 # The default SSID will connect you to
 # the network at the Hack The Badge space.
-WIFI_SSID = "u25-badger-party"
-WIFI_PASSWORD = "h4ck4w4y"
+WIFI_SSID = "github-events"
+WIFI_PASSWORD = "HelloWorld"
 
 # Update with your GitHub username
-GITHUB_USERNAME = ""
+GITHUB_USERNAME = "maxxcrawford"
